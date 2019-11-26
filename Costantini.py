cb8e29456588daeb99a07b9823f65680f4f4ef6c
Your Name <you@example.com>
2019-11-26 09:25:27 +0100

    add my books

--- Costantini.py
+++ Costantini.py
@@ -1,7 +1,8 @@
 from Book import Book
 
-Book_01 = Book("La canzone da sei soldi ","A. J. Cronin","Bompiani",1965 , 373, "IT\\ICCU\\LIA\\0032849")
-Book_02 = Book("La camera cinese", "Vivian Connell", "Garzanti", 1965, 303, "IT\\ICCU\\SBL\\0553196")
+la_canzone_dei_soldi = Book("La canzone da sei soldi ","A. J. Cronin","Bompiani",1965 , 373, "IT\\ICCU\\LIA\\0032849")
+la_camera_cinese = Book("La camera cinese", "Vivian Connell", "Garzanti", 1965, 303, "IT\\ICCU\\SBL\\0553196")
 
-Books = [Book_01, Book_02]
-def(get_books_list)
+books = [la_canzone_dei_soldi, la_camera_cinese]
+def get_books_list():
+    return books
